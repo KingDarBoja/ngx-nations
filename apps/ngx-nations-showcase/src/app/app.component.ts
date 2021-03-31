dbedaed3add48154b7ecf46cb532e2d61e49d999
KingDarBoja <stevenbojato04@gmail.com>
2021-03-31 08:43:27 -0500

    initial commit

---
+++ apps/ngx-nations-showcase/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-nations-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ngx-nations-showcase';
+}
